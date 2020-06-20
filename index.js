666252e10e2a3b1b426b720c77e4b9f66af87b34
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-20 13:34:01 +0200

    use static files

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('public'));
-app.use(express.static('public/script'));
+app.use(express.static('public/font'));
 app.use(express.static('public/style'));
+app.use(express.static('public/script'));
+app.use(express.static('public/media/img'));
+app.use(express.static('public/media/video'));
+app.use(express.static('public/presentation'));
 
 io.on('connection', (socket) => {
     console.log('user connected');
