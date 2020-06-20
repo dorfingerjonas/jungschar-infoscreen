c211457a98a471b04e750e307df435e05514dd33
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-20 17:11:57 +0200

    implement async getWeather() function

--- controller/RequestHandler.js
+++ controller/RequestHandler.js
@@ -1,6 +1,17 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
 class RequestHandler {
     getTime() {
         return new Date().getTime();
+    }
+
+    async getWeather() {
+        const api = require('../data/weatherapi.json');
+        const url = `${api.url}?id=${api.cityId}&appid=${api.apiKey}&units=${api.units}`;
+
+        const response = await fetch(url);
+        return await response.json();
     }
 }
 
