a8c4a31146a1de9128a4b62100ec656557872222
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-20 17:11:19 +0200

    include static folder icons

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const reqHandler = new RequestHandler();
 
 app.use(express.static('public'));
 app.use(express.static('public/font'));
+app.use(express.static('public/icons'));
 app.use(express.static('public/style'));
 app.use(express.static('public/scripts'));
 app.use(express.static('public/media/img'));
