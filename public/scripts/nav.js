05c020efba5d561cd4ea9a5f9fb892a7dc5a8806
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-23 21:07:08 +0200

    add nav.js

---
+++ public/scripts/nav.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', () => {
+    const currentPageBar = document.getElementById('currentPageBar');
+    const elements =[
+        {nav: document.getElementById('navDashboard'), screen: document.getElementById('dashboard')},
+        {nav: document.getElementById('navManageJobs'), screen: document.getElementById('manageJobs')},
+        {nav: document.getElementById('navEditNews'), screen: document.getElementById('editNews')},
+        {nav: document.getElementById('navEditJobs'), screen: document.getElementById('editJobs')},
+        {nav: document.getElementById('navMediaContent'), screen: document.getElementById('mediaContent')},
+    ];
+
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        
+        element.nav.addEventListener('click', () => {
+            currentPageBar.style.left = `${i * 10}vw`;
+            hideAll();
+            element.screen.classList.remove('hide');
+        });
+    }
+
+    function hideAll() {
+        for (const element of elements) {
+            element.screen.classList.add('hide');
+        }
+    }
+});
