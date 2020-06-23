bf1a37444703b11d6e4879fd971ca6941ac5b565
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-06-23 19:46:33 +0200

    implement select() and deselect() workplace functions

--- public/scripts/job.js
+++ public/scripts/job.js
@@ -10,3 +10,50 @@ window.addEventListener('load', () => {
         
     });
 });
+
+function selectWorkplace(jobId) {
+    const icons = document.querySelectorAll(`#job${jobId} .userwrapper svg`);
+
+    if (!document.getElementById(`job${jobId}`).className.includes('jobFull')) {
+        let i = 0;
+        let placeSelected = false;
+
+        while (i < icons.length && !placeSelected) {
+            if (!icons[i].isUsed) {
+                for (const path of icons[i].children) {
+                    path.classList.add('selectedWorkplace');
+                }
+
+                icons[i].isUsed = true;
+                placeSelected = true;
+
+                if (i === icons.length - 1) {
+                    document.getElementById(`job${jobId}`).classList.add('jobFull');
+                }
+            }
+
+            i++;
+        }
+    }
+}
+
+function deselectWorkplace(jobId) {
+    const icons = document.querySelectorAll(`#job${jobId} .userwrapper svg`);
+
+    let i = icons.length - 1;
+    let placeSelected = false;
+
+    while (i >= 0 && !placeSelected) {
+        if (icons[i].isUsed) {
+            for (const path of icons[i].children) {
+                path.classList.remove('selectedWorkplace');
+            }
+
+            icons[i].isUsed = false;
+            placeSelected = true;
+            document.getElementById(`job${jobId}`).classList.remove('jobFull');
+        }
+
+        i--;
+    }
+}
