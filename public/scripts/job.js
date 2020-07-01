106092e17f8905700313e278451212992695c9e6
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-07-01 06:54:19 +0200

    implement printJobs() and add refresh functionality

--- public/scripts/job.js
+++ public/scripts/job.js
@@ -2,6 +2,7 @@ window.addEventListener('load', () => {
     const parent = document.getElementById('jobScreen');
     const socket = getSocket();
     let currency;
+    let icon;
 
     socket.emit('request currency', null);
     socket.emit('request jobs', null);
@@ -12,35 +13,43 @@ window.addEventListener('load', () => {
     });
 
     socket.on('jobs', data => {
-        socket.on('user svg', svg => {
-
-            removeAllChildren(parent);
-            
-            for (const job of data) {
-                const newJob = document.createElement('div');
-                const name = document.createElement('h2');
-                const salary = document.createElement('p');
-                const userWrapper = document.createElement('div');
-
-                name.textContent = job.name;
-                salary.textContent = `Gehalt: ${job.salary} ${currency}`;
-
-                for (let i = 0; i < job.amount; i++) {
-                    userWrapper.innerHTML += svg;
-                    userWrapper.children[i].isUsed = false;
-                }
-
-                userWrapper.classList.add('userWrapper');
-                newJob.classList.add('job');
-                newJob.setAttribute('id', `job${job.id}`);
-                
-                newJob.appendChild(name);
-                newJob.appendChild(salary);
-                newJob.appendChild(userWrapper);
-                parent.appendChild(newJob);
-            }
-        });
+        if (!icon) {            
+            socket.on('user svg', svg => {
+                icon = svg;
+                printJobs(data, svg);
+            });
+        } else {
+            printJobs(data, icon);
+        }
     });
+
+    function printJobs(data, svg) {
+        removeAllChildren(parent);
+                
+        for (const job of data) {
+            const newJob = document.createElement('div');
+            const name = document.createElement('h2');
+            const salary = document.createElement('p');
+            const userWrapper = document.createElement('div');
+    
+            name.textContent = job.name;
+            salary.textContent = `Gehalt: ${job.salary} ${currency}`;
+    
+            for (let i = 0; i < job.amount; i++) {
+                userWrapper.innerHTML += svg;
+                userWrapper.children[i].isUsed = false;
+            }
+    
+            userWrapper.classList.add('userWrapper');
+            newJob.classList.add('job');
+            newJob.setAttribute('id', `job${job.id}`);
+            
+            newJob.appendChild(name);
+            newJob.appendChild(salary);
+            newJob.appendChild(userWrapper);
+            parent.appendChild(newJob);
+        }
+    }
 });
 
 function selectWorkplace(jobId) {
