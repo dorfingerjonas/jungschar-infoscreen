fc0e411116ffe19314ec4b4659a1ec6e2da0f3ce
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-08-21 07:29:48 +0200

    print custom weather

--- public/scripts/weather.js
+++ public/scripts/weather.js
@@ -7,40 +7,44 @@ window.addEventListener('load', () => {
     socket.on('weather', data => {
         removeAllChildren(parent);
 
-        const detailedWrapper = document.createElement('div');
-        const overviewWrapper = document.createElement('div');
-
-        const weatherData = [
-            {name: 'min. Temperatur', value: `${Math.round(data.main.temp_min)}°C`},
-            {name: 'max. Temperatur', value: `${Math.round(data.main.temp_max)}°C`},
-            {name: 'Windgeschwindigkeit', value: `${data.wind.speed}km/h`},
-            {name: 'Luftdruck', value: `${data.main.pressure}hPa`},
-            {name: 'Luftfeuchtigkeit', value: `${data.main.humidity}%`}
-        ];
-
-        const icon = document.createElement('img');
-        icon.src = `../icons/${('0' + parseInt(data.weather[0].icon)).slice(-2)}.svg`;
-        icon.alt = 'Bild konnte nicht geladen werden.';
-
-        const temp = document.createElement('span');
-        temp.textContent = `${parseInt(Math.round(data.main.temp))}°C`;
-
-        for (const data of weatherData) {
-            const newElement = document.createElement('div');
-            const name = document.createElement('span');
-            const value = document.createElement('span');
-
-            name.textContent = data.name;
-            value.textContent = data.value.replace('.', ',');
-
-            newElement.appendChild(name);
-            newElement.appendChild(value);
-            overviewWrapper.appendChild(newElement);
+        if (data !== undefined && data !== null) {
+            const detailedWrapper = document.createElement('div');
+            const overviewWrapper = document.createElement('div');
+    
+            const weatherData = [
+                {name: 'min. Temperatur', value: `${Math.round(data.minTemp)}°C`},
+                {name: 'max. Temperatur', value: `${Math.round(data.maxTemp)}°C`},
+                {name: 'Windgeschwindigkeit', value: `${data.speed}km/h`},
+                {name: 'Luftdruck', value: `${data.pressure}hPa`},
+                {name: 'Luftfeuchtigkeit', value: `${data.humidity}%`}
+            ];
+    
+            const icon = document.createElement('img');
+            icon.src = `../icons/${('0' + parseInt(data.icon)).slice(-2)}.svg`;
+            icon.alt = 'Bild konnte nicht geladen werden.';
+    
+            const temp = document.createElement('span');
+            temp.textContent = `${parseInt(Math.round(data.temp))}°C`;
+    
+            for (const data of weatherData) {
+                const newElement = document.createElement('div');
+                const name = document.createElement('span');
+                const value = document.createElement('span');
+    
+                name.textContent = data.name;
+                value.textContent = data.value.replace('.', ',');
+    
+                newElement.appendChild(name);
+                newElement.appendChild(value);
+                overviewWrapper.appendChild(newElement);
+            }
+    
+            detailedWrapper.appendChild(icon);
+            detailedWrapper.appendChild(temp);
+            parent.appendChild(detailedWrapper);
+            parent.appendChild(overviewWrapper);
+        } else {
+            parent.appendChild(createNoElementsMessage());
         }
-
-        detailedWrapper.appendChild(icon);
-        detailedWrapper.appendChild(temp);
-        parent.appendChild(detailedWrapper);
-        parent.appendChild(overviewWrapper);
     });
 });
