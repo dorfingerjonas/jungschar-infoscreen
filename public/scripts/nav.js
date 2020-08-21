3fe2cdf28d24d48d39ab863e84f12a99c78da5e5
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-08-21 07:28:15 +0200

    add nav element

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,8 +3,9 @@ window.addEventListener('load', () => {
     const elements =[
         {nav: document.getElementById('navDashboard'), screen: document.getElementById('dashboard')},
         {nav: document.getElementById('navManageJobs'), screen: document.getElementById('manageJobs')},
-        {nav: document.getElementById('navEditNews'), screen: document.getElementById('editNews')},
         {nav: document.getElementById('navEditJobs'), screen: document.getElementById('editJobs')},
+        {nav: document.getElementById('navEditNews'), screen: document.getElementById('editNews')},
+        {nav: document.getElementById('navEditWeather'), screen: document.getElementById('editWeather')},
         {nav: document.getElementById('navMediaContent'), screen: document.getElementById('mediaContent')},
     ];
 
@@ -12,7 +13,7 @@ window.addEventListener('load', () => {
         const element = elements[i];
         
         element.nav.addEventListener('click', () => {
-            currentPageBar.style.left = `${i * 14}vw`;
+            currentPageBar.style.left = `${i * 14.16}vw`;
             hideAll();
             element.screen.classList.remove('hide');
         });
